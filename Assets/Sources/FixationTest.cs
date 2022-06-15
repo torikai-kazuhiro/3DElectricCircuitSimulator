713d6723c1e4ac8ab023889c5bc4a70cb7308d84
torikai-kazuhiro <79391154+torikai-kazuhiro@users.noreply.github.com>
2022-06-15 17:45:48 +0900

    init
    init

---
+++ Assets/Sources/FixationTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixationTest : MonoBehaviour
+{
+
+    float right = -4.7f;
+    float left = -4.5f;
+    float top = 0.3f;
+    float lowest = 0.1f;
+    float centerX = -4.71039f;
+    float centerY = 0.25351f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tmp = this.transform.position;
+        if(right< tmp.x && tmp.x < left)//rightとleftの間に来た時
+        {
+            if(lowest < tmp.y && tmp.y < top)//topとlowestの間に来た時
+            {
+                this.transform.position = new Vector3(centerX, centerY, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Sources/RotationTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTest : MonoBehaviour
+{
+    float click;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.rotation = Quaternion.Euler(click, 90.0f, 0.0f);
+        
+
+    }
+    void OnMouseOver()
+    {
+        //オブジェクトの上に右クリックを受け付ける
+        if (Input.GetMouseButtonDown(1))   click += 90;
+    }
+}
